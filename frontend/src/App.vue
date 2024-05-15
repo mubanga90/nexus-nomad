f76db5473519176f9cae1f8c82fc4055bbf5c193
Douwe de Vries <douwemdevries@gmail.com>
2024-05-15 10:05:09 +0200

    [NN-2] Add linting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import ModuleGrid from './components/ModuleGrid/ModuleGrid.vue';
-import { fromTextToModuleGrid } from './utils/gridShapeConverter';
-const gridShape = `
-  100000
-  111110
-  111111
-  110011
-  000011`;
+import StarMap from './components/Modules/StarMap.vue';
 </script>
 
 <template>
-  <div class="centered">
-    <ModuleGrid :moduleGridProp="fromTextToModuleGrid(gridShape)" />
-  </div>
+	<div class="centered">
+		<StarMap />
+	</div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
 .centered {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
 }
 </style>
---
+++ frontend/src/components/Modules/StarMap.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="module starmap">
+		<h1 class="header">StarMap</h1>
+		<div class="content"></div>
+	</div>
+</template>
+
+<style scoped lang="postcss">
+.module {
+	width: 100%;
+	max-width: 100rem;
+	border: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+.header {
+	pointer-events: none;
+	margin: 0;
+	padding: 0;
+	font-size: 1.5rem;
+}
+</style>
